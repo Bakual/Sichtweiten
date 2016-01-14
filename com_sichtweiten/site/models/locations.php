1f37dea902712e650af8d134954bb4097e739f0f
Thomas Hunziker <werbemails@bakual.ch>
2016-01-14 14:17:05 +0100

    Adding locations view

---
+++ com_sichtweiten/site/models/locations.php
@@ -0,0 +1,209 @@
+<?php
+/**
+ * @package     Sichtweiten
+ * @subpackage  Component.Site
+ * @author      Thomas Hunziker <bakual@bakual.ch>
+ * @copyright   2015 - Thomas Hunziker
+ * @license     http://www.gnu.org/licenses/gpl.html
+ **/
+
+defined('_JEXEC') or die();
+
+/**
+ * Model class for the Sichtweiten Component
+ *
+ * @since  1.0
+ */
+class SichtweitenModelLocations extends JModelList
+{
+	/**
+	 * An array of items
+	 *
+	 * @var    array
+	 * @since  1.0
+	 */
+	protected $item;
+
+	/**
+	 * A blacklist of filter variables to not merge into the model's state
+	 *
+	 * @var    array
+	 * @since  1.0
+	 */
+	protected $filterBlacklist = array('period');
+
+	/**
+	 * Constructor.
+	 *
+	 * @param   array $config An optional associative array of configuration settings.
+	 *
+	 * @see     JModelLegacy
+	 * @since   1.0
+	 */
+	public function __construct($config = array())
+	{
+		$this->filter_fields = array(
+			'tp.name',
+			'tp.bemerkungen',
+			'g.displayName',
+		);
+
+		$params = JFactory::getApplication()->getParams();
+
+		// Taken from https://docs.joomla.org/Connecting_to_an_external_database
+		$option = array();
+
+		$option['driver']   = $params->get('db_type', 'mysqli');
+		$option['host']     = $params->get('db_host', 'localhost');
+		$option['database'] = $params->get('db_database');
+		$option['user']     = $params->get('db_user');
+		$option['password'] = $params->get('db_pass');
+		$option['prefix']   = $params->get('db_prefix', 'jos_');
+
+		$config['dbo'] = JDatabaseDriver::getInstance($option);
+
+		parent::__construct($config);
+	}
+
+	/**
+	 * Method to get a JDatabaseQuery object for retrieving the data set from a database.
+	 *
+	 * @return  JDatabaseQuery   A JDatabaseQuery object to retrieve the data set.
+	 *
+	 * @since   1.0
+	 */
+	protected function getListQuery()
+	{
+		// Create a new query object.
+		$db    = $this->getDbo();
+		$query = $db->getQuery(true);
+
+		// Select required fields from the table
+		$query->select(
+			$db->quoteName(
+				array(
+					'tp.id',
+					'tp.name',
+					'tp.bemerkungen',
+					'tp.active',
+				)
+			)
+		);
+
+		$query->from('`#__sicht_tauchplatz` AS tp');
+
+		// Join over Gewaesser table
+		$query->select(
+			array(
+				$db->quoteName('g.name', 'gewaesser_name'),
+				$db->quoteName('g.displayName', 'gewaesser_displayName'),
+			)
+		);
+
+		$query->join('LEFT', '#__sicht_gewaesser AS g ON tp.gewaesser_id = g.id');
+
+		// Join over Land table
+		$query->select(
+			$db->quoteName(
+				array(
+					'lg.bezeichnung',
+					'lg.kurzzeichen',
+				),
+				array(
+					'land_gewaesser_bezeichnung',
+					'land_gewaesser_kurzzeichen',
+				)
+			)
+		);
+
+		$query->join('LEFT', '#__sicht_land AS lg ON g.land_id = lg.id');
+
+		// Join over Bezeichnung table
+		$query->select("GROUP_CONCAT(b.name SEPARATOR ', ') AS alt_name");
+		$query->join('LEFT', '#__sicht_bezeichnung AS b ON tp.id = b.tauchplatz_id');
+		$query->group('tp.id');
+
+		// Join over Ort table
+		$query->select(
+			array(
+				$db->quoteName('o.name', 'ort_name'),
+			)
+		);
+
+		$query->join('LEFT', '#__sicht_ort AS o ON tp.ort_id = o.id');
+
+		// Join over Land table
+		$query->select(
+			$db->quoteName(
+				array(
+					'lo.bezeichnung',
+					'lo.kurzzeichen',
+				),
+				array(
+					'land_ort_bezeichnung',
+					'land_ort_kurzzeichen',
+				)
+			)
+		);
+
+		$query->join('LEFT', '#__sicht_land AS lo ON o.land_id = lo.id');
+
+		// Filter by search in title
+		$search = $this->getState('filter.search');
+
+		if ($search)
+		{
+			$search = $db->quote('%' . $db->escape($search, true) . '%');
+			$query->where($db->quoteName('tp.name') . ' LIKE ' . $search . ')');
+		}
+
+		// Filter by state
+		$state = $this->getState('filter.state');
+
+		if (is_numeric($state))
+		{
+			$query->where($db->quoteName('tp.active') . ' = ' . (int) $state);
+		}
+
+		// Add a hardcoded list ordering clause.
+		$query->order('lg.bezeichnung DESC, g.displayName ASC, tp.name ASC');
+
+		return $query;
+	}
+
+	/**
+	 * Method to auto-populate the model state.
+	 *
+	 * This method should only be called once per instantiation and is designed
+	 * to be called on the first call to the getState() method unless the model
+	 * configuration flag to ignore the request is set.
+	 *
+	 * Note. Calling getState in this method will result in recursion.
+	 *
+	 * @param   string $ordering  An optional ordering field.
+	 * @param   string $direction An optional direction (asc|desc).
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	protected function populateState($ordering = null, $direction = null)
+	{
+		$app    = JFactory::getApplication();
+		$params = $app->getParams();
+		$this->setState('params', $params);
+
+		$user = JFactory::getUser();
+
+		if ((!$user->authorise('core.edit.state', 'com_sichtweiten')) && (!$user->authorise('core.edit', 'com_sichtweiten')))
+		{
+			// Filter on published for those who do not have edit or edit.state rights.
+			$this->setState('filter.state', 1);
+		}
+
+		$search = $app->getUserStateFromRequest($this->context . '.filter.search', 'filter-search', '', 'STRING');
+		$this->setState('filter.search', $search);
+
+		parent::populateState('g.displayName', 'ASC');
+	}
+}
---
+++ com_sichtweiten/site/views/locations/tmpl/default.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @package     Sichtweiten
+ * @subpackage  Component.Site
+ * @author      Thomas Hunziker <bakual@bakual.ch>
+ * @copyright   2015 - Thomas Hunziker
+ * @license     http://www.gnu.org/licenses/gpl.html
+ **/
+
+defined('_JEXEC') or die();
+
+JHtml::stylesheet('com_sichtweiten/sichtweiten.css', '', true);
+?>
+<div class="sichtweiten-container<?php echo htmlspecialchars($this->params->get('pageclass_sfx')); ?>">
+	<div class="items">
+		<?php if (!count($this->items)) : ?>
+			<div class="no_entries alert alert-error"><?php echo JText::sprintf('COM_SICHTWEITEN_NO_ENTRIES', JText::_('COM_SICHTWEITEN_LOCATIONS')); ?></div>
+		<?php else : ?>
+			<?php $land      = ''; ?>
+			<?php $gewaesser = ''; ?>
+			<?php foreach($this->items as $i => $item) : ?>
+				<?php if ($item->gewaesser_name != $gewaesser) : ?>
+					<?php // Close old table if there was already a Gewaesser before ?>
+					<?php if ($gewaesser) : ?>
+						</tbody>
+						</table>
+					<?php endif; ?>
+					<?php if ($item->land_gewaesser_kurzzeichen != $land) : ?>
+						<?php $land = $item->land_gewaesser_kurzzeichen; ?>
+						<h2><?php echo $item->land_gewaesser_bezeichnung; ?></h2>
+					<?php endif; ?>
+					<?php $gewaesser = $item->gewaesser_name; ?>
+					<h3><?php echo $item->gewaesser_displayName; ?></h3>
+					<table class="table table-striped table-hover table-condensed">
+					<thead><tr>
+						<th class="ort"><?php echo JText::_('COM_SICHTWEITEN_FIELD_ORT_LABEL'); ?></th>
+						<th class="location"><?php echo JText::_('COM_SICHTWEITEN_FIELD_LOCATION_LABEL'); ?></th>
+						<th class="alternate"><?php echo JText::_('COM_SICHTWEITEN_FIELD_ALT_NAME_LABEL'); ?></th>
+						<th class="bemerkungen"><?php echo JText::_('COM_SICHTWEITEN_FIELD_BEMERKUNGEN_LABEL'); ?></th>
+					</tr></thead>
+					<tbody>
+				<?php endif; ?>
+				<tr>
+					<td class="ort">
+						<?php echo $item->ort_name; ?>
+					</td>
+					<td class="location">
+						<a href="<?php echo JRoute::_('index.php?option=com_sichtweiten&view=location&id=' . $item->id); ?>">
+							<?php echo $item->name; ?>
+						</a>
+					</td>
+					<td class="alternate">
+						<?php echo $item->alt_name ? htmlspecialchars($item->alt_name) : '-'; ?>
+					</td>
+					<td class="bemerkungen">
+						<?php echo htmlspecialchars($item->bemerkungen); ?>
+					</td>
+				</tr>
+			<?php endforeach; ?>
+			</tbody>
+			</table>
+		<?php endif; ?>
+	</div>
+</div>
