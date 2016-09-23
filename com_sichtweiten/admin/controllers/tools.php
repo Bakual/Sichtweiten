1be8c4d3c92b643ec2530a849eb5556379d4cbbc
Thomas Hunziker <werbemails@bakual.ch>
2016-09-23 12:52:49 +0200

    Data migration from extern DB

---
+++ com_sichtweiten/admin/controllers/tools.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * @package     Sichtweiten
+ * @subpackage  Component.Site
+ * @author      Thomas Hunziker <bakual@bakual.ch>
+ * @copyright   2015 - Thomas Hunziker
+ * @license     http://www.gnu.org/licenses/gpl.html
+ **/
+
+defined('_JEXEC') or die;
+
+/**
+ * Tools Sichtweiten Controller
+ *
+ * @since  1.2.0
+ */
+class SichtweitenControllerTools extends JControllerLegacy
+{
+	/**
+	 * Migrates data from extern database
+	 *
+	 * @throws Exception
+	 *
+	 * @since 1.2.0
+	 */
+	public function migrate()
+	{
+		// Check for request forgeries
+		JSession::checkToken('request') or jexit(JText::_('JINVALID_TOKEN'));
+
+		$app    = JFactory::getApplication();
+		$params = JComponentHelper::getParams('com_sichtweiten');
+
+		if (!$params->get('extern_db'))
+		{
+			$app->enqueueMessage('Extern DB not enabled!');
+			$app->redirect('index.php?option=com_sichtweiten&view=main');
+
+			return;
+		}
+
+		$db = JFactory::getDbo();
+
+		// Taken from https://docs.joomla.org/Connecting_to_an_external_database
+		$option = array();
+
+		$option['driver']   = $params->get('db_type', 'mysqli');
+		$option['host']     = $params->get('db_host', 'localhost');
+		$option['database'] = $params->get('db_database');
+		$option['user']     = $params->get('db_user');
+		$option['password'] = $params->get('db_pass');
+		$option['prefix']   = $params->get('db_prefix', 'jos_');
+
+		$dbExtern = JDatabaseDriver::getInstance($option);
+
+		$tables = array(
+			'#__sicht_adresse',
+			'#__sicht_bezeichnung',
+			'#__sicht_gewaesser',
+			'#__sicht_land',
+			'#__sicht_ort',
+			'#__sicht_sichtweite',
+			'#__sicht_sichtweiteneintrag',
+			'#__sicht_sichtweitenmeldung',
+			'#__sicht_tauchpartner',
+			'#__sicht_tauchplatz',
+			'#__sicht_tiefenbereich',
+			'#__sicht_user',
+		);
+
+		$query = $dbExtern->getQuery(true);
+		$query->select('*');
+
+		foreach ($tables as $table)
+		{
+			$query->clear('from');
+			$query->from($table);
+
+			$dbExtern->setQuery($query);
+			$list = $dbExtern->loadObjectList();
+
+			foreach ($list as $tupel)
+			{
+				$db->insertObject($table, $tupel);
+			}
+		}
+
+		$app->enqueueMessage('Data migrated!');
+		$app->redirect('index.php?option=com_sichtweiten&view=main');
+
+		return;
+	}
+}
--- com_sichtweiten/admin/views/main/tmpl/default.php
+++ com_sichtweiten/admin/views/main/tmpl/default.php
@@ -9,27 +9,41 @@
 
 defined('_JEXEC') or die;
 
+JHtml::_('bootstrap.tooltip');
+$session = JFactory::getSession();
+
 JHtml::stylesheet('administrator/components/com_sichtweiten/sichtweiten.css');
 ?>
 <div id="j-main-container" class="sichtweiten-container">
 	<ul class="thumbnails">
-		<li class="span4">
+		<li class="span6">
 			<a class="thumbnail" href="index.php?option=com_sichtweiten&view=visibilities">
 				<div class="icon"><span class="icon-drawer-2"></span></div>
 				<h3 class="center"><?php echo JText::_('COM_SICHTWEITEN_VISIBILITIES_TITLE'); ?></h3>
 			</a>
 		</li>
+		<li class="span6">
+			<a class="thumbnail" href="index.php?option=com_sichtweiten&view=help">
+				<div class="icon"><span class="icon-help"></span></div>
+				<h3 class="center"><?php echo JText::_('COM_SICHTWEITEN_HELP_TITLE'); ?></h3>
+			</a>
+		</li>
+	</ul>
+	<h3><?php echo JText::_('COM_SICHTWEITEN_STAMMDATEN'); ?></h3>
+	<ul class="thumbnails">
 		<li class="span4">
 			<a class="thumbnail" href="index.php?option=com_sichtweiten&view=divesites">
 				<div class="icon"><span class="icon-location"></span></div>
 				<h3 class="center"><?php echo JText::_('COM_SICHTWEITEN_DIVESITES_TITLE'); ?></h3>
 			</a>
 		</li>
-		<li class="span4">
-			<a class="thumbnail" href="index.php?option=com_sichtweiten&view=help">
-				<div class="icon"><span class="icon-help"></span></div>
-				<h3 class="center"><?php echo JText::_('COM_SICHTWEITEN_HELP_TITLE'); ?></h3>
-			</a>
-		</li>
+		<?php if (JComponentHelper::getParams('com_sichtweiten')->get('extern_db')) : ?>
+			<li class="span4 hasTooltip" title="<?php echo JText::_('COM_SICHTWEITEN_MIGRATE_DESC'); ?>">
+				<a class="thumbnail" href="index.php?option=com_sichtweiten&task=tools.migrate&<?php echo $session->getName() . '=' . $session->getId() . '&' . JSession::getFormToken(); ?>=1">
+					<div class="icon"><span class="icon-database"></span></div>
+					<h3 class="center"><?php echo JText::_('COM_SICHTWEITEN_MIGRATE_TITLE'); ?></h3>
+				</a>
+			</li>
+		<?php endif; ?>
 	</ul>
 </div>
