af8b398d8b45984a9f88d47c4381f791834980ba
Thomas Hunziker <github@bakual.ch>
2022-07-20 15:44:02 +0200

    Main view done with cards

--- com_sichtweiten/admin/views/main/tmpl/default.php
+++ com_sichtweiten/admin/views/main/tmpl/default.php
@@ -15,62 +15,107 @@ use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Session\Session;
 
-HtmlHelper::_('bootstrap.tooltip');
-$session = Factory::getSession();
-HtmlHelper::stylesheet('administrator/components/com_sichtweiten/sichtweiten.css');
 ?>
-<div id="j-main-container" class="sichtweiten-container">
-	<ul class="thumbnails">
-		<li class="span6">
-			<a class="thumbnail" href="index.php?option=com_sichtweiten&view=visibilityreports">
-				<div class="icon"><span class="icon-drawer-2"></span></div>
-				<h3 class="center"><?php echo Text::_('COM_SICHTWEITEN_VISIBILITYREPORTS_TITLE'); ?></h3>
-			</a>
-		</li>
-		<li class="span6">
-			<a class="thumbnail" href="index.php?option=com_sichtweiten&view=help">
-				<div class="icon"><span class="icon-help"></span></div>
-				<h3 class="center"><?php echo Text::_('COM_SICHTWEITEN_HELP_TITLE'); ?></h3>
-			</a>
-		</li>
-	</ul>
-	<h3><?php echo Text::_('COM_SICHTWEITEN_STAMMDATEN'); ?></h3>
-	<ul class="thumbnails">
-		<li class="span2">
-			<a class="thumbnail" href="index.php?option=com_sichtweiten&view=divesites">
-				<div class="icon"><span class="icon-location"></span></div>
-				<h3 class="center"><?php echo Text::_('COM_SICHTWEITEN_DIVESITES_TITLE'); ?></h3>
-			</a>
-		</li>
-		<li class="span2">
-			<a class="thumbnail" href="index.php?option=com_sichtweiten&view=waters">
-				<div class="icon"><span class="icon-flag"></span></div>
-				<h3 class="center"><?php echo Text::_('COM_SICHTWEITEN_WATERS_TITLE'); ?></h3>
-			</a>
-		</li>
-		<li class="span2">
-			<a class="thumbnail" href="index.php?option=com_sichtweiten&view=places">
-				<div class="icon"><span class="icon-compass"></span></div>
-				<h3 class="center"><?php echo Text::_('COM_SICHTWEITEN_PLACES_TITLE'); ?></h3>
-			</a>
-		</li>
-		<li class="span2">
-			<a class="thumbnail" href="index.php?option=com_sichtweiten&view=countries">
-				<div class="icon"><span class="icon-flag-3"></span></div>
-				<h3 class="center"><?php echo Text::_('COM_SICHTWEITEN_COUNTRIES_TITLE'); ?></h3>
-			</a>
-		</li>
-		<li class="span2">
-			<a class="thumbnail" href="index.php?option=com_sichtweiten&view=visibilities">
-				<div class="icon"><span class="icon-eye"></span></div>
-				<h3 class="center"><?php echo Text::_('COM_SICHTWEITEN_VISIBILITIES_TITLE'); ?></h3>
-			</a>
-		</li>
-		<li class="span2 disabled hasTooltip" title="<?php echo Text::_('COM_SICHTWEITEN_CURRENTLY_NOT_USED'); ?>">
-			<a class="thumbnail" href="index.php?option=com_sichtweiten&view=depths">
-				<div class="icon"><span class="icon-arrow-down-4"></span></div>
-				<h3 class="center"><?php echo Text::_('COM_SICHTWEITEN_DEPTHS_TITLE'); ?></h3>
-			</a>
-		</li>
-	</ul>
+<div id="j-main-container" class="j-main-container sichtweiten-container">
+	<div class="row row-cols-1 row-cols-md-2 g-2">
+		<div class="col">
+			<div class="card text-center">
+				<div class="card-header bg-light">
+					<span class="fas fa-folder fa-4x m-auto"></span>
+				</div>
+				<div class="card-body">
+					<a class="stretched-link" href="index.php?option=com_sichtweiten&view=visibilityreports">
+						<h3 class="card-title"><?php echo Text::_('COM_SICHTWEITEN_VISIBILITYREPORTS_TITLE'); ?></h3>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="col">
+			<div class="card text-center">
+				<div class="card-header bg-light">
+					<span class="fas fa-question fa-4x m-auto"></span>
+				</div>
+				<div class="card-body">
+					<a class="stretched-link" href="index.php?option=com_sichtweiten&view=help">
+						<h3 class="card-title"><?php echo Text::_('COM_SICHTWEITEN_HELP_TITLE'); ?></h3>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<h3 class="card card-body my-2 text-center"><?php echo Text::_('COM_SICHTWEITEN_STAMMDATEN'); ?></h3>
+	<div class="row row-cols-1 row-cols-md-3 g-2">
+		<div class="col">
+			<div class="card text-center">
+				<div class="card-header bg-light">
+					<span class="fas fa-map-marker-alt fa-4x m-auto"></span>
+				</div>
+				<div class="card-body">
+					<a class="stretched-link" href="index.php?option=com_sichtweiten&view=divesites">
+						<h3 class="card-title"><?php echo Text::_('COM_SICHTWEITEN_DIVESITES_TITLE'); ?></h3>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="col">
+			<div class="card text-center">
+				<div class="card-header bg-light">
+					<span class="fas fa-flag fa-4x m-auto"></span>
+				</div>
+				<div class="card-body">
+					<a class="stretched-link" href="index.php?option=com_sichtweiten&view=waters">
+						<h3 class="card-title"><?php echo Text::_('COM_SICHTWEITEN_WATERS_TITLE'); ?></h3>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="col">
+			<div class="card text-center">
+				<div class="card-header bg-light">
+					<span class="fas fa-compass fa-4x m-auto"></span>
+				</div>
+				<div class="card-body">
+					<a class="stretched-link" href="index.php?option=com_sichtweiten&view=places">
+						<h3 class="card-title"><?php echo Text::_('COM_SICHTWEITEN_PLACES_TITLE'); ?></h3>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="col">
+			<div class="card text-center">
+				<div class="card-header bg-light">
+					<span class="fas fa-globe-europe fa-4x m-auto"></span>
+				</div>
+				<div class="card-body">
+					<a class="stretched-link" href="index.php?option=com_sichtweiten&view=countries">
+						<h3 class="card-title"><?php echo Text::_('COM_SICHTWEITEN_COUNTRIES_TITLE'); ?></h3>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="col">
+			<div class="card text-center">
+				<div class="card-header bg-light">
+					<span class="fas fa-eye fa-4x m-auto"></span>
+				</div>
+				<div class="card-body">
+					<a class="stretched-link" href="index.php?option=com_sichtweiten&view=visibilities">
+						<h3 class="card-title"><?php echo Text::_('COM_SICHTWEITEN_VISIBILITIES_TITLE'); ?></h3>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="col disabled" title="<?php echo Text::_('COM_SICHTWEITEN_CURRENTLY_NOT_USED'); ?>">
+			<div class="card text-center">
+				<div class="card-header bg-light">
+					<span class="fas fa-arrow-down fa-4x m-auto"></span>
+				</div>
+				<div class="card-body">
+					<a class="stretched-link" href="index.php?option=com_sichtweiten&view=depths">
+						<h3 class="card-title"><?php echo Text::_('COM_SICHTWEITEN_DEPTHS_TITLE'); ?></h3>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
