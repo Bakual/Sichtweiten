bf33c2af9a798635926196a4ec116c318e490982
Thomas Hunziker <github@bakual.ch>
2022-07-20 13:38:43 +0200

    Beginning with visibilities view

--- com_sichtweiten/admin/views/visibilities/tmpl/default.php
+++ com_sichtweiten/admin/views/visibilities/tmpl/default.php
@@ -15,9 +15,7 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\Router\Route;
 
-HTMLHelper::_('bootstrap.tooltip');
 HTMLHelper::_('behavior.multiselect');
-HTMLHelper::_('dropdown.init');
 
 $user      = Factory::getUser();
 $canEdit   = $user->authorise('core.edit', 'com_sichtweiten');
@@ -27,69 +25,69 @@ $listDirn  = $this->state->get('list.direction');
 ?>
 <form action="<?php echo Route::_('index.php?option=com_sichtweiten&view=visibilities'); ?>" method="post"
 	name="adminForm" id="adminForm">
-	<?php if (!empty($this->sidebar)): ?>
-	<div id="j-sidebar-container" class="span2">
-		<?php echo $this->sidebar; ?>
-	</div>
-	<div id="j-main-container" class="span10">
-		<?php else : ?>
-		<div id="j-main-container">
-			<?php endif; ?>
-			<?php echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this, 'options' => array('filterButton' => false))); ?>
-			<?php if (empty($this->items)) : ?>
-				<div class="alert alert-no-items">
-					<?php echo Text::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
-				</div>
-			<?php else : ?>
-				<table class="table table-striped" id="countryList">
-					<thead>
-					<tr>
-						<th width="1%" class="hidden-phone">
-							<input type="checkbox" name="checkall-toggle" value=""
-								title="<?php echo Text::_('JGLOBAL_CHECK_ALL'); ?>"
-								onclick="Joomla.checkAll(this)"/>
-						</th>
-						<th class="nowrap">
-							<?php echo HTMLHelper::_('searchtools.sort', 'JGLOBAL_TITLE', 's.bezeichnung', $listDirn, $listOrder); ?>
-						</th>
-						<th>
-							<?php echo Text::_('COM_SICHTWEITEN_FIELD_DISPLAYNAME_LABEL'); ?>
-						</th>
-						<th width="1%" class="nowrap hidden-phone">
-							<?php echo HTMLHelper::_('searchtools.sort', 'JGRID_HEADING_ID', 's.id', $listDirn, $listOrder); ?>
-						</th>
-					</tr>
-					</thead>
-					<tbody>
-					<?php foreach ($this->items as $i => $item) : ?>
-						<tr class="row<?php echo $i % 2; ?>">
-							<td class="center hidden-phone">
-								<?php echo HTMLHelper::_('grid.id', $i, $item->id); ?>
-							</td>
-							<td>
-								<?php if ($canEdit) : ?>
-									<a href="<?php echo Route::_('index.php?option=com_sichtweiten&task=visibility.edit&id=' . (int) $item->id); ?>">
-										<?php echo $item->bezeichnung; ?></a>
-								<?php else : ?>
-									<?php echo $item->bezeichnung; ?>
-								<?php endif; ?>
-							</td>
-							<td>
-								<?php echo Text::_('COM_SICHTWEITEN_SICHTWEITE_VALUE_' . $item->id); ?>
-							</td>
-							<td class="center hidden-phone">
-								<?php echo (int) $item->id; ?>
-							</td>
-						</tr>
-					<?php endforeach; ?>
-					</tbody>
-				</table>
-			<?php endif; ?>
+	<div class="row">
+		<div class="col-md-12">
+			<div id="j-main-container" class="j-main-container">
+				<?php echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this, 'options' => array('filterButton' => false))); ?>
+				<?php if (empty($this->items)) : ?>
+					<div class="alert alert-info">
+						<span class="fas fa-info-circle" aria-hidden="true"></span><span
+								class="sr-only"><?php echo Text::_('INFO'); ?></span>
+						<?php echo Text::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
+					</div>
+				<?php else : ?>
+					<table class="table" id="visibilitiesList">
+						<thead>
+							<tr>
+								<td style="width:1%" class="text-center">
+									<?php echo HTMLHelper::_('grid.checkall'); ?>
+								</td>
+								<th scope="col" style="min-width:100px">
+									<?php echo HTMLHelper::_('searchtools.sort', 'JGLOBAL_TITLE', 's.bezeichnung', $listDirn, $listOrder); ?>
+								</th>
+								<th scope="col" style="width:10%" class="d-none d-md-table-cell">
+									<?php echo Text::_('COM_SICHTWEITEN_FIELD_DISPLAYNAME_LABEL'); ?>
+								</th>
+								<th scope="col" style="width:3%" class="d-none d-lg-table-cell">
+									<?php echo HTMLHelper::_('searchtools.sort', 'JGRID_HEADING_ID', 's.id', $listDirn, $listOrder); ?>
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+						<?php foreach ($this->items as $i => $item) : ?>
+							<tr class="row<?php echo $i % 2; ?>">
+								<td class="text-center">
+									<?php echo HTMLHelper::_('grid.id', $i, $item->id, false, 'cid', 'cb', $item->title); ?>
+								</td>
+								<td class="nowrap has-context">
+									<?php if ($canEdit) : ?>
+										<a href="<?php echo Route::_('index.php?option=com_sichtweiten&task=visibility.edit&id=' . (int) $item->id); ?>"
+										   title="<?php echo Text::_('JACTION_EDIT'); ?> <?php echo $this->escape(addslashes($item->title)); ?>">>
+											<?php echo $item->bezeichnung; ?>
+										</a>
+									<?php else : ?>
+										<?php echo $item->bezeichnung; ?>
+									<?php endif; ?>
+								</td>
+								<td class="small d-none d-md-table-cell">
+									<?php echo Text::_('COM_SICHTWEITEN_SICHTWEITE_VALUE_' . $item->id); ?>
+								</td>
+								<td class="d-none d-lg-table-cell">
+									<?php echo (int) $item->id; ?>
+								</td>
+							</tr>
+						<?php endforeach; ?>
+						</tbody>
+					</table>
 
-			<?php echo $this->pagination->getListFooter(); ?>
+					<?php echo $this->pagination->getListFooter(); ?>
 
-			<input type="hidden" name="task" value=""/>
-			<input type="hidden" name="boxchecked" value="0"/>
-			<?php echo HTMLHelper::_('form.token'); ?>
+				<?php endif; ?>
+
+				<input type="hidden" name="task" value=""/>
+				<input type="hidden" name="boxchecked" value="0"/>
+				<?php echo HTMLHelper::_('form.token'); ?>
+			</div>
 		</div>
+	</div>
 </form>
