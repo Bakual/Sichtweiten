e19b9310d7c96ce8d49ce91b80720e163c377caa
Thomas Hunziker <werbemails@bakual.ch>
2023-07-26 16:14:08 +0200

    Starting some J4 adjustement

--- com_sichtweiten/admin/views/visibility/tmpl/edit.php
+++ com_sichtweiten/admin/views/visibility/tmpl/edit.php
@@ -3,37 +3,29 @@
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\Router\Route;
 
 defined('_JEXEC') or die;
 
 // Load the tooltip behavior.
 HTMLHelper::_('bootstrap.tooltip');
-HTMLHelper::_('behavior.formvalidation');
 HTMLHelper::_('behavior.keepalive');
+
+$this->useCoreUI = true;
 
 $app   = Factory::getApplication();
 $input = $app->input;
 ?>
 
-<script type="text/javascript">
-	Joomla.submitbutton = function (task) {
-		if (task === 'visibility.cancel' || document.formvalidator.isValid(document.id('adminForm'))) {
-			Joomla.submitform(task, document.getElementById('adminForm'));
-		} else {
-			alert('<?php echo $this->escape(Text::_('JGLOBAL_VALIDATION_FORM_FAILED'));?>');
-		}
-	}
-</script>
-
 <form action="<?php echo Route::_('index.php?option=com_sichtweiten&layout=edit&id=' . (int) $this->item->id); ?>"
-	method="post" name="adminForm" id="adminForm" class="form-validate">
-	<div class="form-horizontal">
-		<?php foreach($this->form->getFieldset('general') as $field): ?>
-			<?php echo $field->getControlGroup(); ?>
-		<?php endforeach; ?>
-		<input type="hidden" name="task" value=""/>
-		<input type="hidden" name="return" value="<?php echo $input->getCmd('return'); ?>"/>
+	  method="post" name="adminForm" id="item-form" class="form-validate">
+	<div class="main-card">
+		<?php echo HTMLHelper::_('uitab.startTabSet', 'myTab'); ?>
+
+		<?php echo LayoutHelper::render('joomla.edit.params', $this); ?>
+		<input type="hidden" name="task" value="">
+		<input type="hidden" name="return" value="<?php echo $input->getBase64('return'); ?>">
 		<?php echo HTMLHelper::_('form.token'); ?>
 	</div>
 </form>
