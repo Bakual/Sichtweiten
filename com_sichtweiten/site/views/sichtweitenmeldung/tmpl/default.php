3131bb6c08cc3220a435394f69cc6874e9a59442
Thomas Hunziker <github@bakual.ch>
2023-11-17 13:53:49 +0100

    Moving buttons to bottom of Sichtweitenmeldung layout sovles #1

--- com_sichtweiten/site/views/sichtweitenmeldung/tmpl/default.php
+++ com_sichtweiten/site/views/sichtweitenmeldung/tmpl/default.php
@@ -31,14 +31,6 @@ HTMLHelper::stylesheet('com_sichtweiten/sichtweiten.css', ['relative' => true]);
 
 <div class="sichtweiten-container<?php echo $this->pageclass_sfx; ?> edit item-page<?php echo $this->pageclass_sfx; ?>">
 	<form action="<?php echo Route::_('index.php?option=com_sichtweiten&view=sichtweitenmeldung'); ?>" method="post" name="adminForm" id="adminForm" class="form form-validate form-horizontal">
-		<div class="mb-2">
-			<button type="button" class="btn btn-primary" onclick="Joomla.submitbutton('sichtweitenmeldung.save')">
-				<span class="icon-check"></span> <?php echo Text::_('JSAVE') ?>
-			</button>
-			<button type="button" class="btn btn-danger" onclick="Joomla.submitbutton('sichtweitenmeldung.cancel')">
-				<span class="icon-times"></span> <?php echo Text::_('JCANCEL') ?>
-			</button>
-		</div>
 		<fieldset>
 			<?php foreach($this->form->getFieldset('general') as $field): ?>
 				<?php echo $this->form->renderField($field->fieldname); ?>
@@ -47,6 +39,14 @@ HTMLHelper::stylesheet('com_sichtweiten/sichtweiten.css', ['relative' => true]);
 			<input type="hidden" name="return" value="<?php echo $this->return_page; ?>" />
 			<?php echo HTMLHelper::_('form.token'); ?>
 		</fieldset>
+		<div class="mb-2">
+			<button type="button" class="btn btn-primary" onclick="Joomla.submitbutton('sichtweitenmeldung.save')">
+				<span class="icon-check"></span> <?php echo Text::_('JSAVE') ?>
+			</button>
+			<button type="button" class="btn btn-danger" onclick="Joomla.submitbutton('sichtweitenmeldung.cancel')">
+				<span class="icon-times"></span> <?php echo Text::_('JCANCEL') ?>
+			</button>
+		</div>
 	</form>
 	<?php if ($this->params->get('copyright')) : ?>
 		<div class="copyright"><small><?php echo Text::_('COM_SICHTWEITEN_COPYRIGHT'); ?></small></div>
