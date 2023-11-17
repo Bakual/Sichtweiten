8abca9802fc98e3d42eaddc933a2832acfdc8f5f
Thomas Hunziker <github@bakual.ch>
2023-11-17 15:36:19 +0100

    Languages are already loaded with fallback by Joomla

--- com_sichtweiten/site/sichtweiten.php
+++ com_sichtweiten/site/sichtweiten.php
@@ -15,11 +15,6 @@ defined('_JEXEC') or die;
 // Register Helperclasses for autoloading
 JLoader::discover('SichtweitenHelper', JPATH_COMPONENT . '/helpers');
 
-// Load languages and merge with fallbacks
-$jlang = Factory::getLanguage();
-$jlang->load('com_sichtweiten', JPATH_COMPONENT, 'en-GB', true);
-$jlang->load('com_sichtweiten', JPATH_COMPONENT, null, true);
-
 $controller = BaseController::getInstance('Sichtweiten');
 $controller->execute(Factory::getApplication()->input->get('task'));
 $controller->redirect();
