25f1261d41c321312dc2d62034722bf879996009
Thomas Hunziker <github@bakual.ch>
2023-11-23 07:36:24 +0100

    Remove sublayouts

--- com_sichtweiten/site/views/visibilities/tmpl/default.php
+++ com_sichtweiten/site/views/visibilities/tmpl/default.php
@@ -12,23 +12,123 @@ defined('_JEXEC') or die();
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Router\Route;
 
 $listOrder = $this->state->get('list.ordering');
 $listDirn  = $this->state->get('list.direction');
 
+HTMLHelper::_('bootstrap.tooltip');
+
 HTMLHelper::stylesheet('com_sichtweiten/sichtweiten.css', ['relative' => true]);
+
+// Prepare Accordion
+$accordionOptions = array();
+
+if ($gewaesser = Factory::getApplication()->getInput()->getInt('gewaesser', 0))
+{
+	$accordionOptions['active'] = 'collapse' . $gewaesser;
+}
 ?>
 <div class="sichtweiten-container<?php echo htmlspecialchars($this->params->get('pageclass_sfx', '')); ?>">
 	<div class="items">
-		<form action="<?php echo htmlspecialchars(Uri::getInstance()->toString()); ?>" method="post" id="adminForm" name="adminForm">
+		<form action="<?php echo htmlspecialchars(Uri::getInstance()->toString()); ?>" method="post" id="adminForm"
+			  name="adminForm">
 			<?php if (!count($this->gewaesser)) : ?>
 				<div class="no_entries alert alert-error"><?php echo Text::_('JGLOBAL_NO_MATCHING_RESULTS'); ?></div>
 			<?php else : ?>
- 				<?php echo $this->loadTemplate('gewaesser'); ?>
+				<?php echo HTMLHelper::_('bootstrap.startAccordion', 'gewaesserAccordion', $accordionOptions); ?>
+				<?php foreach ($this->gewaesser as $see) : ?>
+					<div class="mb-3 bg-light">
+						<?php $title = $see->displayName; ?>
+						<?php if ($see->land_kurzzeichen != 'CH') : ?>
+							<?php $title .= '&nbsp;<small>(' . $see->land_bezeichnung . ')</small>'; ?>
+						<?php endif; ?>
+						<?php echo HTMLHelper::_('bootstrap.addSlide', 'gewaesserAccordion', $title, 'collapse' . $see->id); ?>
+						<table class="table table-light table-hover">
+							<thead>
+							<tr>
+								<th class="ort"><?php echo HTMLHelper::_('grid.sort', 'COM_SICHTWEITEN_FIELD_LOCATION_LABEL', 'tp.name', $listDirn, $listOrder); ?></th>
+								<th class="datum"><?php echo HTMLHelper::_('grid.sort', 'JDATE', 'datum', $listDirn, $listOrder); ?></th>
+								<th class="tiefe"><?php echo HTMLHelper::_('grid.sort', 'COM_SICHTWEITEN_FIELD_TIEFENBEREICH0_LABEL', 'sichtweite_id_0', $listDirn, $listOrder); ?></th>
+								<th class="tiefe"><?php echo HTMLHelper::_('grid.sort', 'COM_SICHTWEITEN_FIELD_TIEFENBEREICH1_LABEL', 'sichtweite_id_1', $listDirn, $listOrder); ?></th>
+								<th class="tiefe"><?php echo HTMLHelper::_('grid.sort', 'COM_SICHTWEITEN_FIELD_TIEFENBEREICH2_LABEL', 'sichtweite_id_2', $listDirn, $listOrder); ?></th>
+								<th class="tiefe"><?php echo HTMLHelper::_('grid.sort', 'COM_SICHTWEITEN_FIELD_TIEFENBEREICH3_LABEL', 'sichtweite_id_3', $listDirn, $listOrder); ?></th>
+								<th class="tiefe"><?php echo HTMLHelper::_('grid.sort', 'COM_SICHTWEITEN_FIELD_TIEFENBEREICH4_LABEL', 'sichtweite_id_4', $listDirn, $listOrder); ?></th>
+								<th class="tiefe"><?php echo HTMLHelper::_('grid.sort', 'COM_SICHTWEITEN_FIELD_TIEFENBEREICH5_LABEL', 'sichtweite_id_5', $listDirn, $listOrder); ?></th>
+								<th class="kommentar"><?php echo HTMLHelper::_('grid.sort', 'COM_SICHTWEITEN_FIELD_KOMMENTAR_LABEL', 'kommentar', $listDirn, $listOrder); ?></th>
+								<th class="user"><?php echo HTMLHelper::_('grid.sort', 'COM_SICHTWEITEN_USER', 'user_id', $listDirn, $listOrder); ?></th>
+							</tr>
+							</thead>
+							<tbody>
+							<?php foreach ($see->visibilities as $item) : ?>
+								<tr>
+									<td class="ort">
+										<a href="<?php echo Route::_('index.php?option=com_sichtweiten&view=location&id=' . $item->id); ?>"
+										   class="hasTooltip"
+										   title="<?php echo Text::_('COM_SICHTWEITEN_FIELD_ORT_LABEL') . ': ' . $item->ort_name; ?><br />
+											<?php echo Text::_('COM_SICHTWEITEN_FIELD_LAND_LABEL') . ': ' . $item->land_ort_bezeichnung; ?>">
+											<?php echo $item->name; ?>
+										</a>
+									</td>
+									<td class="datum">
+										<?php echo HTMLHelper::_('date', $item->datum, Text::_('DATE_FORMAT_LC4'), 'UTC'); ?>
+									</td>
+									<td class="tiefe sichtweite<?php echo $item->sichtweite_id_0; ?>">
+										<?php echo Text::_('COM_SICHTWEITEN_SICHTWEITE_VALUE_' . $item->sichtweite_id_0); ?>
+									</td>
+									<td class="tiefe sichtweite<?php echo $item->sichtweite_id_1; ?>">
+										<?php echo Text::_('COM_SICHTWEITEN_SICHTWEITE_VALUE_' . $item->sichtweite_id_1); ?>
+									</td>
+									<td class="tiefe sichtweite<?php echo $item->sichtweite_id_2; ?>">
+										<?php echo Text::_('COM_SICHTWEITEN_SICHTWEITE_VALUE_' . $item->sichtweite_id_2); ?>
+									</td>
+									<td class="tiefe sichtweite<?php echo $item->sichtweite_id_3; ?>">
+										<?php echo Text::_('COM_SICHTWEITEN_SICHTWEITE_VALUE_' . $item->sichtweite_id_3); ?>
+									</td>
+									<td class="tiefe sichtweite<?php echo $item->sichtweite_id_4; ?>">
+										<?php echo Text::_('COM_SICHTWEITEN_SICHTWEITE_VALUE_' . $item->sichtweite_id_4); ?>
+									</td>
+									<td class="tiefe sichtweite<?php echo $item->sichtweite_id_5; ?>">
+										<?php echo Text::_('COM_SICHTWEITEN_SICHTWEITE_VALUE_' . $item->sichtweite_id_5); ?>
+									</td>
+									<td class="kommentar">
+										<?php $kommentar = $item->kommentar ?: ''; ?>
+										<?php $kommentar = htmlspecialchars($kommentar); ?>
+										<?php $kommentar_truncated = HTMLHelper::_('string.truncate', $kommentar, $this->params->get('kommentar_length', 50)); ?>
+										<?php if ($kommentar_truncated === $kommentar) : ?>
+											<?php echo $kommentar_truncated; ?>
+										<?php else : ?>
+											<span title="<?php echo $kommentar; ?>"
+												  class="hasTooltip"><?php echo $kommentar_truncated; ?></span>
+										<?php endif; ?>
+										<?php if ($item->buddy_names) : ?>
+											<?php if ($kommentar) : ?>
+												|
+											<?php endif; ?>
+											<?php echo Text::_('COM_SICHTWEITEN_BUDDIES'); ?>:
+											<?php echo SichtweitenHelperSichtweiten::getBuddies($item->buddy_names, $item->buddy_emails); ?>
+										<?php endif; ?>
+									</td>
+									<td class="user">
+										<?php if ($item->user_id) : ?>
+											<a href="<?php echo Route::_('index.php?option=com_sichtweiten&view=user&id=' . $item->user_id); ?>">
+												<?php echo $item->user_name; ?>
+											</a>
+										<?php endif; ?>
+									</td>
+								</tr>
+							<?php endforeach; ?>
+							</tbody>
+						</table>
+						<?php echo HTMLHelper::_('bootstrap.endSlide'); ?>
+					</div>
+				<?php endforeach; ?>
+				<?php echo HTMLHelper::_('bootstrap.endAccordion'); ?>
 			<?php endif; ?>
-			<input type="hidden" name="task" value="" />
-			<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
-			<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
+			<input type="hidden" name="task" value=""/>
+			<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>"/>
+			<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>"/>
 		</form>
 	</div>
 	<?php if ($this->params->get('copyright')) : ?>
