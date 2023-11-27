f915406facc150a77d9ec8f61e878e5484c0cb89
Thomas Hunziker <github@bakual.ch>
2023-11-27 09:57:06 +0100

    Change database

---
+++ com_sichtweiten/admin/sql/updates/mysql/2.1.1.sql
@@ -0,0 +1,27 @@
+ALTER TABLE `#__sicht_tauchplatz`
+    ADD COLUMN (
+        `alias` VARCHAR(400) DEFAULT '' NOT NULL,
+        `alt_names` TEXT NULL
+    ),
+    CHANGE COLUMN `name` `title` VARCHAR(255) DEFAULT '' NOT NULL,
+    CHANGE COLUMN `active` `state` TINYINT DEFAULT 0 NOT NULL,
+    CHANGE COLUMN `ersetztdurch_id` `modified_by` INT UNSIGNED NULL,
+    CHANGE COLUMN `erfasstvon_id` `created_by` INT UNSIGNED NULL,
+    DROP COLUMN `oldKey`,
+    DROP COLUMN `fuelllstation_id`
+;
+ALTER TABLE `#__sicht_sichtweite`
+    ADD COLUMN (
+        `value` TINYINT DEFAULT 0 NOT NULL,
+        `ordering` TINYINT DEFAULT 0 NOT NULL,
+        `languagestring` TINYTEXT NULL
+        ),
+    CHANGE COLUMN `bezeichnung` `title` VARCHAR(255) DEFAULT '' NOT NULL
+;
+ALTER TABLE `#__sicht_tiefenbereich`
+    ADD COLUMN (
+        `ordering` TINYINT DEFAULT 0 NOT NULL,
+        `languagestring` TINYTEXT NULL
+        ),
+    CHANGE COLUMN `bezeichnung` `title` VARCHAR(255) DEFAULT '' NOT NULL
+;
