f1e005365c98e3e1db2b72eeed2e90182bd22d6c
Thomas Hunziker <github@bakual.ch>
2023-11-27 12:07:16 +0100

    Joomla doesn't like aggregating multiple add commands into one. It works, but shows an error in the database manager.

--- com_sichtweiten/admin/sql/updates/mysql/2.1.1.sql
+++ com_sichtweiten/admin/sql/updates/mysql/2.1.1.sql
@@ -1,8 +1,6 @@
 ALTER TABLE `#__sicht_tauchplatz`
-    ADD COLUMN (
-        `alias` VARCHAR(400) DEFAULT '' NOT NULL,
-        `alt_names` TEXT NULL
-    ),
+    ADD COLUMN `alias` VARCHAR(400) DEFAULT '' NOT NULL,
+    ADD COLUMN `alt_names` TEXT NULL,
     CHANGE COLUMN `name` `title` VARCHAR(255) DEFAULT '' NOT NULL,
     CHANGE COLUMN `active` `state` TINYINT DEFAULT 0 NOT NULL,
     CHANGE COLUMN `ersetztdurch_id` `modified_by` INT UNSIGNED NULL,
@@ -11,17 +9,13 @@ ALTER TABLE `#__sicht_tauchplatz`
     DROP COLUMN `fuelllstation_id`
 ;
 ALTER TABLE `#__sicht_sichtweite`
-    ADD COLUMN (
-        `value` TINYINT DEFAULT 0 NOT NULL,
-        `ordering` TINYINT DEFAULT 0 NOT NULL,
-        `languagestring` TINYTEXT NULL
-        ),
+    ADD COLUMN `value` TINYINT DEFAULT 0 NOT NULL,
+    ADD COLUMN `ordering` TINYINT DEFAULT 0 NOT NULL,
+    ADD COLUMN `languagestring` TINYTEXT NULL,
     CHANGE COLUMN `bezeichnung` `title` VARCHAR(255) DEFAULT '' NOT NULL
 ;
 ALTER TABLE `#__sicht_tiefenbereich`
-    ADD COLUMN (
-        `ordering` TINYINT DEFAULT 0 NOT NULL,
-        `languagestring` TINYTEXT NULL
-        ),
+    ADD COLUMN `ordering` TINYINT DEFAULT 0 NOT NULL,
+    ADD COLUMN `languagestring` TINYTEXT NULL,
     CHANGE COLUMN `bezeichnung` `title` VARCHAR(255) DEFAULT '' NOT NULL
 ;
